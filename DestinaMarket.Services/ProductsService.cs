9250ee2307017476247f06fe02ea21d090c33c06
DESKTOP-8DA08I3\Mehmet Kadir <mkadirkuzucu@gmail.com>
2019-12-04 00:16:57 +0300

    Products

---
+++ DestinaMarket.Services/ProductsService.cs
@@ -0,0 +1,59 @@
+﻿using DestinaMarket.Database;
+using DestinaMarket.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestinaMarket.Services
+{
+    public class ProductsService
+    {
+        
+        public Product GetProduct(int ID)
+        {
+            using (var context = new DMContext())
+            {
+                return context.Products.Find(ID);
+            }
+        }
+
+        public List<Product> GetProducts()
+        {
+            using (var context = new DMContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        public void SaveProduct(Product product)
+        {
+            using(var context=new DMContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new DMContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteProduct(int ID)
+        {
+            using (var context = new DMContext())
+            {
+                var product = context.Products.Find(ID);
+
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DestinaMarket.Web/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using DestinaMarket.Entities;
+using DestinaMarket.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DestinaMarket.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productService = new ProductsService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductTable()
+        {
+            var products = productService.GetProducts();
+
+            return View(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productService.SaveProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+    }
+}
